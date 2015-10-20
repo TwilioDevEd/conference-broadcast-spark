a3d47c9eafd71d89f4483bddfe252a8faf8928b6
Mario Celi <mceli@stackbuilders.com>
2015-10-20 12:00:39 -0500

    Add create call test

--- src/main/java/com/twilio/conferencebroadcast/controllers/RecordingController.java
+++ src/main/java/com/twilio/conferencebroadcast/controllers/RecordingController.java
@@ -4,10 +4,12 @@ import com.twilio.conferencebroadcast.exceptions.UndefinedEnvironmentVariableExc
 import com.twilio.conferencebroadcast.lib.AppSetup;
 import com.twilio.conferencebroadcast.lib.RecordingUriTransformer;
 import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
 import com.twilio.sdk.resource.instance.Recording;
 import com.twilio.sdk.resource.list.RecordingList;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import spark.Request;
 import spark.Route;
 
 import java.text.SimpleDateFormat;
@@ -39,19 +41,34 @@ public class RecordingController {
   };
 
   public Route create = (request, response) -> {
+    int status = createRecording(request);
+    response.status(status);
+    return "";
+  };
+
+  public int createRecording(Request request) {
     Map params = new HashMap<>();
     String phoneNumber = request.queryParams("phone_number");
-    String twilioNumber = appSetup.getTwilioPhoneNumber();
+    String twilioNumber = null;
+    try {
+      twilioNumber = appSetup.getTwilioPhoneNumber();
+    } catch (UndefinedEnvironmentVariableException e) {
+      e.printStackTrace();
+    }
     String path = request.url().replace(request.uri(), "") + "/broadcast/record";
 
     params.put("From", twilioNumber);
     params.put("To", phoneNumber);
     params.put("Url", path);
 
-    client.getAccount().getCallFactory().create(params);
-
-    return "";
-  };
+    try {
+      client.getAccount().getCallFactory().create(params);
+    } catch (TwilioRestException e) {
+      System.out.println("Twilio rest client error");
+      return e.getErrorCode();
+    }
+    return 200;
+  }
 
   public String getRecordingsAsJSON() {
     RecordingList recordings = client.getAccount().getRecordings();
--- src/test/java/com/twilio/conferencebroadcast/controllers/RecordingControllerTest.java
+++ src/test/java/com/twilio/conferencebroadcast/controllers/RecordingControllerTest.java
@@ -1,7 +1,10 @@
 package com.twilio.conferencebroadcast.controllers;
 
+import com.twilio.conferencebroadcast.exceptions.UndefinedEnvironmentVariableException;
 import com.twilio.conferencebroadcast.lib.AppSetup;
 import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.factory.CallFactory;
 import com.twilio.sdk.resource.instance.Account;
 import com.twilio.sdk.resource.instance.Recording;
 import com.twilio.sdk.resource.list.RecordingList;
@@ -10,15 +13,17 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.junit.Test;
+import spark.Request;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
 public class RecordingControllerTest {
   @Test
@@ -65,5 +70,31 @@ public class RecordingControllerTest {
     assertThat(obj.size(), is(2));
     assertThat(firstElement.get("url"), is("https://api.twilio.com/some/uri"));
     assertThat(firstElement.get("date"), is(dateFormat.format(newDate)));
+  }
+
+  @Test
+  public void createRecordingTest()
+      throws TwilioRestException, UndefinedEnvironmentVariableException {
+    AppSetup mockAppSetup = mock(AppSetup.class);
+    TwilioRestClient mockClient = mock(TwilioRestClient.class);
+    Account mockAccount = mock(Account.class);
+    CallFactory mockCallFactory = mock(CallFactory.class);
+    Request mockRequest = mock(Request.class);
+
+    RecordingController controller = new RecordingController(mockAppSetup, mockClient);
+
+    when(mockClient.getAccount()).thenReturn(mockAccount);
+    when(mockAccount.getCallFactory()).thenReturn(mockCallFactory);
+    when(mockRequest.queryParams("phone_number")).thenReturn("+number");
+    when(mockRequest.url()).thenReturn("some_url");
+    when(mockRequest.uri()).thenReturn("some_uri");
+    when(mockAppSetup.getTwilioPhoneNumber()).thenReturn("+twilio_number");
+
+    int status = controller.createRecording(mockRequest);
+
+    verify(mockRequest).queryParams("phone_number");
+    verify(mockCallFactory).create(any(Map.class));
+
+    assertThat(status, is(200));
   }
 }
